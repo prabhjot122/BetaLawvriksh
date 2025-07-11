3f8a03c6cd735766ce7b8acc05831a5b18480034
prabhjot122 <128220378+prabhjot122@users.noreply.github.com>
2025-07-11 14:01:58 +0530

    Delete Client directory

--- Client/src/vite-env.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="vite/client" />
-/// <reference types="react" />
-/// <reference types="react-dom" />
-
-declare namespace JSX {
-  interface IntrinsicElements {
-    [elemName: string]: any;
-  }
-}
